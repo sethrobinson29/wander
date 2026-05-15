7090ab47c1fbda878cbd79d2fd2e4acefd216eb6
Seth <sethpatrickrobinson@gmail.com>
2026-05-15 11:06:32 -0500

    Add missing import

--- tests/Wander.Tests/Infrastructure/DeactivatedUserTests.cs
+++ tests/Wander.Tests/Infrastructure/DeactivatedUserTests.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.SignalR;
 using Moq;
 using Npgsql;
 using Quartz;
